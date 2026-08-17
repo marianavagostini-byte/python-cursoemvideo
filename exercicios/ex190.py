c5527ce6f0be308006104cd9c9a5c0c611e85b03
marianavagostini-byte <marianavagostini@gmail.com>
2026-08-17 19:38:33 -0300

    Desafio 109: parametro de formatacao nas funcoes

--- exercicios/ex190.py
+++ exercicios/ex190.py
@@ -4,7 +4,7 @@
 from moeda import metade, dobro, aumentar, diminuir, moeda
 
 num = float(input('Digite um preço: R$ '))
-print(f'A metade de {moeda(num)} é {moeda(metade(num))}')
-print(f'O dobro de {moeda(num)} é {moeda(dobro(num))}')
-print(f'Aumentando 10% temos {moeda(aumentar(num, 10))}')
-print(f'Reduzindo 13% temos {moeda(diminuir(num, 13))}')
+print(f'A metade de {moeda(num)} é {metade(num, True)}')
+print(f'O dobro de {moeda(num)} é {dobro(num, True)}')
+print(f'Aumentando 10% temos {aumentar(num, 10,True)}')
+print(f'Reduzindo 13% temos {diminuir(num, 13,True)}')
--- exercicios/moeda.py
+++ exercicios/moeda.py
@@ -1,20 +1,21 @@
 # moeda - Modulo de funcoes financeiras (usado nos desafios 107 a 112)
 
-def dobro(n):
-    num=n*2
-    return num 
 
-def metade(n):
+def metade(n , format=False):
     num=n/2
-    return num 
+    return num if format is False else moeda(num)
 
-def aumentar(n,taxa):
+def dobro(n,format=False):
+    num=n*2
+    return num if format is False else moeda(num)
+
+def aumentar(n,taxa,format=False):
     num=n+n*(taxa/100)
-    return num 
+    return num if format is False else moeda(num)
 
-def diminuir(n,taxa):
+def diminuir(n,taxa,format=False):
     num=n-n*(taxa/100)
-    return num
+    return num if format is False else moeda(num)
 
 def moeda(n):
     num=f'R${n:.2f}'.replace('.',',')
