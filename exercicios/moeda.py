84a4ef738eca8a97e2253782151a7b77d3720f06
marianavagostini-byte <marianavagostini@gmail.com>
2026-08-17 15:40:08 -0300

    Desafio 107: modulo moeda

---
+++ exercicios/moeda.py
@@ -0,0 +1,17 @@
+# moeda - Modulo de funcoes financeiras (usado nos desafios 107 a 112)
+
+def dobro(n):
+    num=n*2
+    return num 
+
+def metade(n):
+    num=n/2
+    return num 
+
+def aumentar(n,taxa):
+    num=n+n*(taxa/100)
+    return num 
+
+def diminuir(n,taxa):
+    num=n-n*(taxa/100)
+    return num
