c668f2ed2a493ad1af89700466ea4eb06073d439
marianavagostini-byte <marianavagostini@gmail.com>
2026-08-27 15:49:17 -0300

    Revisao.03.py

---
+++ exercicios/revisao.03.py
@@ -0,0 +1,23 @@
+
+class Contador:
+    def __init__(self):
+        self.contagem = 0
+
+    def somar(self):
+        self.contagem += 1
+
+    def zerar(self):
+        self.contagem = 0
+
+    def mostrar(self):
+        print(f"Contagem: {self.contagem}")
+
+
+
+c1 = Contador()
+
+c1.somar()
+
+c1.mostrar() 
+
+c1.mostrar()  
